46d1a1feff9a00ba87a29747ee502ce8c0d0fc25
summerness <39141862@qq.com>
2015-06-23 14:28:39 +0800

    Initial Commit

---
+++ jibuqi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jibuqi
+//
+//  Created by summer on 15/6/23.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jibuqi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jibuqi
+//
+//  Created by summer on 15/6/23.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
